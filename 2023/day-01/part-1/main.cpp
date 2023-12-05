5e4d4be0158f359548df089a770489943e96229e
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2023-12-05 16:53:19 -0500

    add EditorConfig

--- 2023/day-01/part-1/main.cpp
+++ 2023/day-01/part-1/main.cpp
@@ -5,61 +5,61 @@ using namespace std;
 
 inline bool is_number(char c)
 {
-    return c >= '0' && c <= '9';
+  return c >= '0' && c <= '9';
 }
 
 inline int to_number(char c)
 {
-    return c - '0';
+  return c - '0';
 }
 
 int main()
 {
 #ifdef __test__
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
 #endif
 
-    string line;
-    int sumTotal = 0;
+  string line;
+  int sumTotal = 0;
 
-    while (cin >> line)
+  while (cin >> line)
+  {
+    string::iterator beginIterator = line.begin();
+    string::iterator endIterator = line.end() - 1;
+    short firstDigit = NULL, secondDigit = NULL;
+
+    while (beginIterator <= endIterator)
     {
-        string::iterator beginIterator = line.begin();
-        string::iterator endIterator = line.end() - 1;
-        short firstDigit = NULL, secondDigit = NULL;
+      char beginChar = *beginIterator, endChar = *endIterator;
 
-        while (beginIterator <= endIterator)
-        {
-            char beginChar = *beginIterator, endChar = *endIterator;
+      if (firstDigit == NULL)
+      {
+        if (is_number(beginChar))
+          firstDigit = to_number(beginChar);
+        else
+          beginIterator++;
+      }
 
-            if (firstDigit == NULL)
-            {
-                if (is_number(beginChar))
-                    firstDigit = to_number(beginChar);
-                else
-                    beginIterator++;
-            }
+      if (secondDigit == NULL)
+      {
+        if (is_number(endChar))
+          secondDigit = to_number(endChar);
+        else
+          endIterator--;
+      }
 
-            if (secondDigit == NULL)
-            {
-                if (is_number(endChar))
-                    secondDigit = to_number(endChar);
-                else
-                    endIterator--;
-            }
-
-            if (firstDigit != NULL && secondDigit != NULL)
-                break;
-        }
-
-        short numInLine = firstDigit * 10 + secondDigit;
-        sumTotal += numInLine;
-
-        cout << line << ": " << numInLine << endl;
+      if (firstDigit != NULL && secondDigit != NULL)
+        break;
     }
 
-    cout << sumTotal;
+    short numInLine = firstDigit * 10 + secondDigit;
+    sumTotal += numInLine;
 
-    return 0;
+    cout << line << ": " << numInLine << endl;
+  }
+
+  cout << sumTotal;
+
+  return 0;
 }
--- 2023/day-02/part-1/main.cpp
+++ 2023/day-02/part-1/main.cpp
@@ -15,57 +15,57 @@ map<string, int> BAG_CONFIGURATION = {
 int main()
 {
 #ifdef __test__
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
 #endif
 
-    string line;
-    int sum_id = 0;
+  string line;
+  int sum_id = 0;
 
-    while (getline(cin, line))
+  while (getline(cin, line))
+  {
+    int game_id;
+    bool is_game_valid = true;
+
+    istringstream iss(line);
+
+    iss.ignore(4); // "Game"
+    iss >> game_id;
+    iss.ignore(1); // ":"
+
+    while (!iss.eof() && is_game_valid)
     {
-        int game_id;
-        bool is_game_valid = true;
+      char divider;
+      string color_name;
+      int num_cubes;
 
-        istringstream iss(line);
+      do
+      {
+        iss >> num_cubes >> color_name;
 
-        iss.ignore(4); // "Game"
-        iss >> game_id;
-        iss.ignore(1); // ":"
+        divider = color_name.back();
 
-        while (!iss.eof() && is_game_valid)
+        if (divider == ',' || divider == ';')
+          color_name.pop_back();
+
+        int max_cubes = BAG_CONFIGURATION[color_name];
+
+        if (num_cubes > max_cubes)
         {
-            char divider;
-            string color_name;
-            int num_cubes;
-
-            do
-            {
-                iss >> num_cubes >> color_name;
-
-                divider = color_name.back();
-
-                if (divider == ',' || divider == ';')
-                    color_name.pop_back();
-
-                int max_cubes = BAG_CONFIGURATION[color_name];
-
-                if (num_cubes > max_cubes)
-                {
-                    is_game_valid = false;
-                    break;
-                }
-            } while (divider == ',');
+          is_game_valid = false;
+          break;
         }
-
-        if (is_game_valid)
-        {
-            cout << "game_id: " << game_id << endl;
-            sum_id += game_id;
-        }
+      } while (divider == ',');
     }
 
-    cout << sum_id << endl;
+    if (is_game_valid)
+    {
+      cout << "game_id: " << game_id << endl;
+      sum_id += game_id;
+    }
+  }
 
-    return 0;
+  cout << sum_id << endl;
+
+  return 0;
 }
