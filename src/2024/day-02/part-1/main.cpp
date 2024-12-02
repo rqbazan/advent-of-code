c6fbc2d374bc3039534ff2bf44e56dec53ddb829
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-12-02 06:52:08 -0500

    fix it

--- src/2024/day-02/part-1/main.cpp
+++ src/2024/day-02/part-1/main.cpp
@@ -23,14 +23,21 @@ int main()
     while (iss >> num)
       report.push_back(num);
 
-    bool is_safe = true;
+    bool is_safe = true,
+         is_all_decreasing = report[0] < report[1];
+
     for (size_t i = 1; i < report.size(); i++)
     {
-      int diff = abs(report[i - 1] - report[i]);
-      bool is_safe = diff <= 1 && diff <= 3;
+      int distance = abs(report[i - 1] - report[i]);
 
-      if (!is_safe)
+      bool is_valid_distance = distance >= 1 && distance <= 3;
+      bool is_decreasing = report[i - 1] < report[i];
+
+      if (!is_valid_distance || is_all_decreasing != is_decreasing)
+      {
+        is_safe = false;
         break;
+      }
     }
 
     if (is_safe)
