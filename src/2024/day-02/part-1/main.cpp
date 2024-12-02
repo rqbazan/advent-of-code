4a5026ced681d1f81a9563899a6f4a468a23e932
rcrd <raquirozbazan@gmail.com>
2024-12-02 06:53:56 -0500

    solve: 2024/day-02/part-1 (#7)

---
+++ src/2024/day-02/part-1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#define __test__
+
+using namespace std;
+
+int main()
+{
+#ifdef __test__
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+#endif
+
+  string line;
+  int total_safe_reports = 0;
+
+  while (getline(cin, line))
+  {
+    int num;
+    istringstream iss(line);
+    vector<int> report;
+
+    while (iss >> num)
+      report.push_back(num);
+
+    bool is_safe = true,
+         is_all_decreasing = report[0] < report[1];
+
+    for (size_t i = 1; i < report.size(); i++)
+    {
+      int distance = abs(report[i - 1] - report[i]);
+
+      bool is_valid_distance = distance >= 1 && distance <= 3;
+      bool is_decreasing = report[i - 1] < report[i];
+
+      if (!is_valid_distance || is_all_decreasing != is_decreasing)
+      {
+        is_safe = false;
+        break;
+      }
+    }
+
+    if (is_safe)
+      total_safe_reports++;
+  }
+
+  cout << total_safe_reports << endl;
+
+  return 0;
+}
