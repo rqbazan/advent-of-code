585bfc0dfb6e806a45bb08acd5dcae4404994155
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2025-12-11 21:17:12 -0500

    solve(2025): solve day 5 - part 1

---
+++ src/2025/day-05/part-1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+#define __test__
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+#ifdef __test__
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+#endif
+
+  vector<pair<ll, ll>> ranges;
+  string line;
+  ll id, start, end;
+  char dash;
+
+  while (getline(cin, line) && !line.empty())
+  {
+    stringstream ss(line);
+    ss >> start >> dash >> end;
+    ranges.push_back({start, end});
+  }
+
+  sort(ranges.begin(), ranges.end());
+
+  int valid = 0;
+  while (cin >> id)
+  {
+    for (const auto &range : ranges)
+    {
+      if (id >= range.first && id <= range.second)
+      {
+        valid++;
+        break;
+      }
+    }
+  }
+
+  cout << valid << endl;
+
+  return 0;
+}
